c219ca074c746d10a6ea6458a06531be0aeb1a88
KhesrawFarid <khesrawfarid@gmail.com>
2026-04-22 18:36:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: /'Wirtschaftprofi'/
+   base: '/Wirtschaftprofi/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(
